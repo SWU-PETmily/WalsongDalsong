b88d4587d69293975fea90db0e0c46ae5e8806b1
whatody <106987822+whatody@users.noreply.github.com>
2022-09-25 23:16:14 +0900

    ui 변경했습니다
    식사급수 위치 바꿨습니다

---
+++ Assets/Scripts/01.Stage1/FoodMove.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class FoodMove : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public static Vector2 defaultposition;
+    public GameObject obj;
+    public float speed = 10.0f;
+    private Transform imPos;
+    void Start()
+    {
+        imPos = obj.GetComponent<RectTransform>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        defaultposition = this.transform.position;
+        RotatingImage();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 currentPos = Input.mousePosition;
+        this.transform.position = currentPos;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+
+        this.transform.position = defaultposition;
+
+    }
+
+    public void RotatingImage()
+    {
+        transform.rotation = Quaternion.Euler(new Vector3(45, 45, speed * Time.deltaTime));
+    }
+}
