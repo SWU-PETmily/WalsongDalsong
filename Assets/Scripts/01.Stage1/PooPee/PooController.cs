9db56e459139dcfbd1cfe4144581d99b4890c3ee
Minjeong Kim <serak0310@naver.com>
2022-09-28 05:06:04 +0900

    [add] 배변 쓰레기통 충돌 설정

---
+++ Assets/Scripts/01.Stage1/PooPee/PooController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PooController : MonoBehaviour
+{
+
+    GameObject trashCan;
+
+    // 바꿀 이미지
+    public Sprite img_garbage_pre;
+    public Sprite img_garbage_open;
+    public Sprite img_garbage;
+
+    void Start()
+    {
+        this.trashCan = GameObject.Find("img_garbage_pre");
+    }
+
+    void Update()
+    {
+        // 충돌 판정
+        Vector2 p1 = transform.position;                // 배변 중심 좌표
+        Vector2 p2 = this.trashCan.transform.position;  // 쓰레기통 중심 좌표
+        Vector2 dir = p1 - p2;
+        float d = dir.magnitude;
+        float r1 = 0.8f;                                // 배변 반경
+        float r2 = 1.2f;                                // 쓰레기통 중심 반경
+
+        if (d < r1 + r2)
+        {
+            // 충돌 시 배변 삭제
+            Destroy(gameObject);
+            this.trashCan.GetComponent<SpriteRenderer>().sprite = this.img_garbage;
+
+        }
+
+    }
+}
