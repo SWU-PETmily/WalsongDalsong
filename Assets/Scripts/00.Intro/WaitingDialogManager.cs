373e318cf1b2a6cb2241e6d3f488a47cacde12bd
Minjeong Kim <serak0310@naver.com>
2022-10-12 02:59:24 +0900

    [ADD] 불안부분 가상부모 대화 추가

--- Assets/Scripts/00.Intro/WaitingDialogManager.cs
+++ Assets/Scripts/00.Intro/WaitingDialogManager.cs
@@ -26,6 +26,9 @@ public class WaitingDialogManager : MonoBehaviour
                 dialogText.text = "우리를 반기지 않는다고 성급히 다가가거나 쓰다듬으려고 하면 사람을 더 무서워할 수 있으니 30분만 기다려보자!";
                 break;
             case 2:
+                dialogText.text = "30분 동안 게임을 종료하고 기다려주면 돼. 중간에 게임을 실행하면 강아지가 겁 먹어서 처음부터 다시 기다려야 하니 주의해줘!";
+                break;
+            case 3:
                 dialogBox.SetActive(false);
                 UnityEngine.SceneManagement.SceneManager.LoadScene("CalmingSignal");
                 break;
