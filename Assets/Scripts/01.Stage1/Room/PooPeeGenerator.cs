df1c509ecf30140496bc98c8e3701367217f4274
Minjeong Kim <serak0310@naver.com>
2022-11-09 04:04:49 +0900

    [ADD] 배소변 발생 조건 수정

--- Assets/Scripts/01.Stage1/Room/PooPeeGenerator.cs
+++ Assets/Scripts/01.Stage1/Room/PooPeeGenerator.cs
@@ -6,11 +6,11 @@ using System;
 
 public class PooPeeGenerator : MonoBehaviour
 {
+    //public DateTime nowTime;
+    int exeTime;                         // 실행한 시간
+    float firstTime = 30.0f;            // 경과시간. 30초 후
+    float waitTime = 60.0f;            // 경과시간. 60초 후
 
-    public DateTime nowTime;
-    public int hh;
-    public int mm;
-    public int ss;
 
     // 배소변 프리팹
     public GameObject poo1;
@@ -18,24 +18,48 @@ public class PooPeeGenerator : MonoBehaviour
     public GameObject pee1;
     public GameObject pee2;
 
-    // 코루틴
-    public bool isDelay;
-    public float delayTime = 3300.0f;   // 55분 동안 딜레이
-    public float accumTime;
-
     // Start is called before the first frame update
     void Start()
     {
-        
+        // 실행한 시간 저장
+        exeTime = int.Parse(System.DateTime.Now.ToString("HHmm"));
+        if (exeTime % 2 == 1)
+        {
+            // 배변 코루틴 시작
+            StartCoroutine("PooRoutine");
+        }
+        else
+        {
+            // 소변 코루틴 시작
+            StartCoroutine("PeeRoutine");
+        }
+    
     }
 
-    // Update is called once per frame
-    void Update()
+    // 게임 시작 후 30초 뒤 배소변
+    IEnumerator PooRoutine()
     {
-        PooPeeTimer();
-
+        yield return new WaitForSeconds(firstTime);              // 첫 시작 대기
+        while (true)
+        {
+            Instantiate(poo1, transform.position, transform.rotation);          // 루틴 대기
+            yield return new WaitForSeconds(waitTime);
+        }
     }
 
+    // 게임 시작 후 30초 뒤 배소변
+    IEnumerator PeeRoutine()
+    {
+        yield return new WaitForSeconds(firstTime);              // 첫 시작 대기
+        while (true)
+        {
+            Instantiate(pee1, transform.position, transform.rotation);          // 루틴 대기
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+
+
+    /*
     // 배소변 시간 계산 함수
     void PooPeeTimer()
     {
@@ -70,5 +94,6 @@ public class PooPeeGenerator : MonoBehaviour
                     break;
             }
         }
-    }
+   
+    } */
 }
